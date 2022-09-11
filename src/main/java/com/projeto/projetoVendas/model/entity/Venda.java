087332c513445ec3bce91bd3e7310edb2fdb4bff
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2022-09-11 17:01:31 -0300

    alterei o metodo total()

--- src/main/java/com/projeto/projetoVendas/model/entity/Venda.java
+++ src/main/java/com/projeto/projetoVendas/model/entity/Venda.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -20,15 +21,44 @@ import javax.persistence.Table;
 @Table(name = "tb_venda")
 public class Venda {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date data;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public List<ItemVenda> getItemvenda() {
+        return itemvenda;
+    }
+
+    public void setItemvenda(List<ItemVenda> itemvenda) {
+        this.itemvenda = itemvenda;
+    }
     
     @OneToMany(mappedBy = "venda")
     private List<ItemVenda> itemvenda;
     
     public Double total(){
-    return null;
+        double total=0;
+        for(int i=0;itemvenda.size()>i;i++){
+            total = itemvenda.get(i).total() + total;
+        }
+    return total;
     }
     
 }

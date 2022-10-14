85ea3a8143000966e84519c41ad2130b95860eeb
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2022-10-13 23:13:37 -0300

    mudei as configurações do SpringMVC

--- src/main/java/com/projeto/projetoVendas/ConfiguracaoSpringMvc.java
+++ src/main/java/com/projeto/projetoVendas/ConfiguracaoSpringMvc.java
@@ -10,8 +10,8 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * @Configuration para indicar ao Spring que essa é uma classe de configuração.
- * Em seguida, é preciso estender a classe WebMvcConfigurerAdapter.
+ * @Configuration indica ao Spring que essa é uma classe de configuração.
+ * Em seguida, é preciso estender a classe WebMvcConfigurer.
  * @author fagno
  */
 @Configuration
@@ -19,16 +19,13 @@ public class ConfiguracaoSpringMvc implements WebMvcConfigurer{
 
     
     /**
-     * Com a chamada a registry.addViewController(), estamos registrando um controller automático,
-     * definido pelo próprio Spring, para atender a requisições direcionadas à URL / e /home. E com a chamada 
-     * a setViewName(), sempre que a aplicação receber uma requisição para um desses endereços, a view home, 
-     * criada na última aula, será exibida.
+     * Com a chamada a registry.addViewController(), estamos registrando um controller automático.
+     * Com o método setViewName(), indicamos a view para atender a requisições direcionadas à URL / e /home. 
      * @param registry 
      */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        //registry.addViewController("/").setViewName("home");
-        registry.addRedirectViewController("/", "/produtos/index");
+        registry.addViewController("/").setViewName("home");
         registry.addViewController("/home").setViewName("home");
 
     }
